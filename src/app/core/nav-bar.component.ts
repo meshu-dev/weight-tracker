d3ce59d1421ab29b69f95dd27dafca5fa41f77db
meshu-dev <admin@requiredev.com>
2019-09-14 12:23:55 +0100

    Added nav bar files and removed default angular template html

---
+++ src/app/core/nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  template: '',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent {
+  title = 'weight-tracker';
+}

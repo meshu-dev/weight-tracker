81ee5e1a510b008ee8cd71ab62607b15d544ed24
meshu-dev <admin@requiredev.com>
2020-01-18 10:15:56 +0000

    Redirect from login page if logged in

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -5,12 +5,10 @@ import { User } from './user';
 import { Router } from '@angular/router';
 import { first } from 'rxjs/operators';
 
-
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
 export class LoginComponent implements OnInit {
   public username: string;
   public password: string;
@@ -23,13 +21,10 @@ export class LoginComponent implements OnInit {
 
   constructor(private authService: AuthService, 
               private router: Router) { }
-
   
   ngOnInit() {
-    //console.log('INIT!!!', this.authService.isLoggedIn);
-
     if (this.authService.isLoggedIn()) {
-      //this.router.navigate(['/']);
+      this.router.navigate(['/']);
     }
   }
 
@@ -37,20 +32,11 @@ export class LoginComponent implements OnInit {
   	console.log('in onsubmit!!!');
     console.log(this.user);
 
-    /*
-    this.userService.login(this.user).subscribe(
-      result => this.onHttpSuccess(result),
-      error => console.log('error: ', error)
-    ); */
-
     this.authService.login(this.user)
       .pipe(first())
       .subscribe(
         result => this.router.navigate(['/']),
         err => this.error = 'Could not authenticate'
       );
-
-    console.log('Config!!!');
-    //console.log(this.userService.getConfig());
   }
 }

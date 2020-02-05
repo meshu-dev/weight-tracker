7c175f41f391422badc20e6ebbd355287477a513
meshu-dev <admin@requiredev.com>
2020-02-05 19:00:55 +0000

    Added web server to handle requests

---
+++ server.js
@@ -0,0 +1,13 @@
+const express    = require('express'),
+	  app        = express(),
+	  path       = require('path');
+
+app.use(express.static(__dirname + '/dist/weight-tracker-ui'));
+
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname + '/dist/weight-tracker-ui/index.html'));
+ });
+
+// Start server
+app.listen(port);
+console.log('Server started on port ' + port);
